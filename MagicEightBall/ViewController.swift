43be1fe898565b52757ea3925879604b4ef699d8
admin <admin@admins-air.attlocal.net>
2018-09-06 22:39:24 -0700

    View Controller

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let answers = ["Yes, definitely", "No", "Without a doubt", "Possibly", "Try again", "Very doubtful", "Don't count on it", "Sure"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var shakeButton: UIButton!
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        generateAnswer()
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        generateAnswer()
+        
+    }
+    func generateAnswer() {
+        let maxIndex = UInt32(answers.count)
+        let randomIndex = Int(arc4random_uniform(maxIndex))
+        answerLabel.text = answers[randomIndex]
+    }
 }
 
